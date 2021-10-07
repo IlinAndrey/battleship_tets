79333ffa559157f88a67b1a9da85754c921a15c9
Andrew <frayz2507@gmail.com>
2021-10-08 01:17:46 +0300

    edit settings

--- game/battleship.py
+++ game/battleship.py
@@ -2,11 +2,11 @@ from random import randint
 import os
 
 
-row_size = 9  # кол-во строк
-col_size = 9  # кол-во столбцов
+row_size = 10  # кол-во строк
+col_size = 10  # кол-во столбцов
 num_ships = 4
-max_ship_size = 5
-min_ship_size = 2
+max_ship_size = 4
+min_ship_size = 1
 num_turns = 40
 
 ship_list = []
