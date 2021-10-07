02d72002b683ecd52a57e11559f37e9c88d6e0f6
Andrew <frayz2507@gmail.com>
2021-10-08 01:39:41 +0300

    add random location

--- game/battleship.py
+++ game/battleship.py
@@ -1,7 +1,6 @@
 from random import randint
 import os
 
-
 row_size = 10  # кол-во строк
 col_size = 10  # кол-во столбцов
 num_ships = 4
@@ -13,11 +12,13 @@ ship_list = []
 board = [[0] * col_size for x in range(row_size)]
 board_display = [["O"] * col_size for x in range(row_size)]
 
+
 def print_board(board_array):
     print("\n  " + " ".join(str(x) for x in range(1, col_size + 1)))
     for r in range(row_size):
         print(str(r + 1) + " " + " ".join(str(c) for c in board_array[r]))
     print()
+
 
 def search_locations(size, orientation):
     locations = []
@@ -42,3 +43,15 @@ def search_locations(size, orientation):
         return 'None'
     else:
         return locations
+
+
+def random_location():
+    size = randint(min_ship_size, max_ship_size)
+    orientation = 'horizontal' if randint(0, 1) == 0 else 'vertical'
+
+    locations = search_locations(size, orientation)
+    if locations == 'None':
+        return 'None'
+    else:
+        return {'позиция': locations[randint(0, len(locations) - 1)], 'размер': size, \
+                'ориентация': orientation}
