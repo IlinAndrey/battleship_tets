d43ae87d74e2c7e5f96d2db315a410b1c6a4df56
Andrew <frayz2507@gmail.com>
2021-10-08 01:22:44 +0300

    function print a board

--- game/battleship.py
+++ game/battleship.py
@@ -12,3 +12,9 @@ num_turns = 40
 ship_list = []
 board = [[0] * col_size for x in range(row_size)]
 board_display = [["O"] * col_size for x in range(row_size)]
+
+def print_board(board_array):
+    print("\n  " + " ".join(str(x) for x in range(1, col_size + 1)))
+    for r in range(row_size):
+        print(str(r + 1) + " " + " ".join(str(c) for c in board_array[r]))
+    print()
